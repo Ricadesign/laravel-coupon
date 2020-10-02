022ccece24fd08162ba7a76bbeb126ddb7f1fc9b
David <dluque@rica.design>
2020-10-02 17:21:54 +0200

    Add api route

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('coupon', 'Ricadesign\LaravelCoupon\CouponController@searchCoupon')->middleware(['web', 'auth']);
+
+Route::group(['prefix' => 'api', 'middleware' => 'api'], function() {
+    Route::get('coupon', 'Ricadesign\LaravelCoupon\CouponController@searchCoupon');
+});
