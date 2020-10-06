00b56b498137e5f368e6d24830ff29ccd3d2e130
David <dluque@rica.design>
2020-10-06 10:25:02 +0200

    Throttle requests

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,3 @@
 <?php
 
-Route::get('coupon', 'Ricadesign\LaravelCoupon\CouponController@searchCoupon')->middleware(['web', 'auth']);
-
-Route::group(['prefix' => 'api', 'middleware' => 'api'], function() {
-    Route::get('coupon', 'Ricadesign\LaravelCoupon\CouponController@searchCoupon');
-});
+Route::get('coupon', 'Ricadesign\LaravelCoupon\CouponController@searchCoupon')->middleware(['web', 'throttle:60,1']);
