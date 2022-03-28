f7b648b23c7bed3ddb04ec5b869d2fe60369742f
acecilia <acecilia@rica.design>
2022-03-28 12:34:08 +0200

    feat: Check duration and apply coupon

--- config/config.php
+++ config/config.php
@@ -2,5 +2,6 @@
 
 return [
   'unique_email' => env('LARAVEL_COUPON_UNIQUE_EMAIL', false),
-  'coupon_for_product' => env('LARAVEL_COUPON_COUPON_FOR_PRODUCT', false)
+  'coupon_for_product' => env('LARAVEL_COUPON_COUPON_FOR_PRODUCT', false),
+  'min_duration' => env('LARAVEL_COUPON_MIN_DURATION', false)
 ];
