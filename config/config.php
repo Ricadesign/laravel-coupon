03709e997edd8a8d1015bafab099640208ed44b0
Joel Pérez <jperez@rica.design>
2025-03-28 13:48:48 +0100

    feat: coupon refund

--- config/config.php
+++ config/config.php
@@ -3,5 +3,6 @@
 return [
   'unique_email' => env('LARAVEL_COUPON_UNIQUE_EMAIL', false),
   'coupon_for_product' => env('LARAVEL_COUPON_COUPON_FOR_PRODUCT', false),
-  'min_type' => env('LARAVEL_COUPON_MIN_TYPE', false)
+  'min_type' => env('LARAVEL_COUPON_MIN_TYPE', false),
+  'open_refund' => env('LARAVEL_COUPON_OPEN_REFUND', false)
 ];
